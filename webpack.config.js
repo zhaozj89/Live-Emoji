74a87d91c866d3aeaf235a7effb0223dcf2002ba
hirosesuzu <zzhaoao@connect.ust.hk>
2017-12-15 12:24:07 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+var path = require('path');
+
+module.exports = {
+  // define entry point
+  entry: './src/js/app.js',
+
+  // define output point
+  output: {
+    path: 'dist',
+    filename: 'bundle.js'
+  },
+
+
+
+};
