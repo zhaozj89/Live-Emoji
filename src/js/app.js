23272aa956250f02d432291a8403603387c585ed
hirosesuzu <zzhaoao@connect.ust.hk>
2017-12-18 22:26:05 +0800

    Add pixijs4.6.2, previous commit is wrong

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,9 @@
 import {createGraphics} from "./graphics";
+import {createGraphics2} from "./graphics2";
 
 require('../css/app.css');
 
 import {createNodeEditor} from "./node_editor";
 
 createNodeEditor();
-createGraphics();
+createGraphics2();
