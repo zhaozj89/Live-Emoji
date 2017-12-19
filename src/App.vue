0e5d437e0800a23498d28d1ea1c4f9be88f4bc29
hirosesuzu <zzhaoao@connect.ust.hk>
2017-12-19 21:33:10 +0800

    A light start version

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view/>
+    <HelloWorld/>
   </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    HelloWorld
+  }
 }
 </script>
 
