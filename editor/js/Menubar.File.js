d997e54ca28fada2f9ea16ed846f6609f8f67754
DESKTOP-A9OL67R\zhaoz <zzhaoao@connect.ust.hk>
2017-12-23 21:54:24 +0800

    Update to threejs R89

--- editor/js/Menubar.File.js
+++ editor/js/Menubar.File.js
@@ -2,65 +2,65 @@
  * @author mrdoob / http://mrdoob.com/
  */
 
-Menubar.File = function ( editor ) {
+Menubar.File = function (editor) {
 
 	var signals = editor.signals;
 
 	var container = new UI.Panel();
-	container.setClass( 'menu' );
+	container.setClass('menu');
 
 	var title = new UI.Panel();
-	title.setClass( 'title' );
-	title.setTextContent( 'File' );
-	container.add( title );
+	title.setClass('title');
+	title.setTextContent('File');
+	container.add(title);
 
 	var options = new UI.Panel();
-	options.setClass( 'options' );
-	container.add( options );
+	options.setClass('options');
+	container.add(options);
 
 	// New
 
 	var option = new UI.Row();
-	option.setClass( 'option' );
-	option.setTextContent( 'New' );
-	option.onClick( function () {
+	option.setClass('option');
+	option.setTextContent('New');
+	option.onClick(function () {
 
-		if ( confirm( 'Any unsaved data will be lost. Are you sure?' ) ) {
+		if (confirm('Any unsaved data will be lost. Are you sure?')) {
 
 			editor.clear();
 
 		}
 
-	} );
-	options.add( option );
+	});
+	options.add(option);
 
 	// import
 
 	var option = new UI.Panel();
-	option.setClass( 'option' );
-	option.setTextContent( 'Import' );
-	option.onClick( Import );
-	options.add( option );
+	option.setClass('option');
+	option.setTextContent('Import');
+	option.onClick(Import);
+	options.add(option);
 
-	var fileInput = document.createElement( 'input' );
+	var fileInput = document.createElement('input');
 	fileInput.type = 'file';
-	fileInput.addEventListener( 'change', function ( event ) {
+	fileInput.addEventListener('change', function (event) {
 
 		var reader = new FileReader();
-		reader.addEventListener( 'load', function ( event ) {
+		reader.addEventListener('load', function (event) {
 
 			editor.clear();
-			editor.fromJSON( JSON.parse( event.target.result ) );
+			editor.fromJSON(JSON.parse(event.target.result));
 
-		}, false );
+		}, false);
 
-		reader.readAsText( fileInput.files[ 0 ] );
+		reader.readAsText(fileInput.files[0]);
 
-	} );
+	});
 
-	function Import () {
+	function Import() {
 
-		if ( confirm( 'Any unsaved data will be lost. Are you sure?' ) )
+		if (confirm('Any unsaved data will be lost. Are you sure?'))
 			fileInput.click();
 
 	}
@@ -68,21 +68,21 @@ Menubar.File = function ( editor ) {
 	// export
 
 	var option = new UI.Panel();
-	option.setClass( 'option' );
-	option.setTextContent( 'Export' );
-	option.onClick( Export );
-	options.add( option );
+	option.setClass('option');
+	option.setTextContent('Export');
+	option.onClick(Export);
+	options.add(option);
 
-	signals.exportState.add( Export );
+	signals.exportState.add(Export);
 
-	function Export () {
+	function Export() {
 
-		var output = JSON.stringify( editor.toJSON(), null, '\t' );
+		var output = JSON.stringify(editor.toJSON(), null, '\t');
 
-		var blob = new Blob( [ output ], { type: 'text/plain' } );
-		var objectURL = URL.createObjectURL( blob );
+		var blob = new Blob([output], {type: 'text/plain'});
+		var objectURL = URL.createObjectURL(blob);
 
-		window.open( objectURL, '_blank' );
+		window.open(objectURL, '_blank');
 		window.focus();
 
 	}
