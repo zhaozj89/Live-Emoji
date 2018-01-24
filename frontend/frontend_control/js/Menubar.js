51b38640c96f1f5ae7c29d295ecb0a63aca77d04
hirosesuzu <zzhaoao@connect.ust.hk>
2018-01-24 16:41:39 +0800

    Return back to my own implementation of node editor

--- frontend/frontend_control/js/Menubar.js
+++ frontend/frontend_control/js/Menubar.js
@@ -9,7 +9,7 @@ var Menubar = function ( editor ) {
 
 	container.add( new Menubar.File( editor ) );
 	container.add( new Menubar.Edit( editor ) );
-	container.add( new Menubar.Add( editor ) );
+	container.add( new Menubar.View( editor ) );
 	container.add( new Menubar.Help( editor ) );
 
 	container.add( new Menubar.Status( editor ) );
