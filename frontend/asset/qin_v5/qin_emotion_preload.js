1ae457f539f5ee821d5eb39bd76a04e0c110335e
hirosesuzu <zzhaoao@connect.ust.hk>
2018-03-08 12:23:10 +0800

    Update girl asset, but short of things

---
+++ frontend/asset/qin_v5/qin_emotion_preload.js
@@ -0,0 +1,71 @@
+var PRELOAD_JSON = {
+	"accessory" : "images/accessory.png",
+	"face" : "images/face.png",
+	"left_ear" : "images/left_ear.png",
+	"left_eye" :
+	{
+		"angry" : "images/left_eye/angry.png",
+		"fearful" : "images/left_eye/fearful.png",
+		"happy" : "images/left_eye/happy.png",
+		"neutral" : "images/left_eye/neutral.png",
+		"sad" : "images/left_eye/sad.png",
+		"surprised" : "images/left_eye/surprised.png",
+		"disgusted": "images/left_eye/disgusted.png",
+		"close": "images/left_eye/close.png"
+	},
+	"left_eyebrow" :
+	{
+		"angry" : "images/left_eyebrow/angry.png",
+		"fearful" : "images/left_eyebrow/fearful.png",
+		"happy" : "images/left_eyebrow/happy.png",
+		"neutral" : "images/left_eyebrow/neutral.png",
+		"sad" : "images/left_eyebrow/sad.png",
+		"surprised" : "images/left_eyebrow/surprised.png",
+		"disgusted": "images/left_eyebrow/disgusted.png"
+	},
+	"left_red" : "images/left_red.png",
+	"mouth" :
+	{
+		"angry" : "images/mouth/angry.png",
+		"fearful" : "images/mouth/fearful.png",
+		"happy" : "images/mouth/happy.png",
+		"neutral" : "images/mouth/neutral.png",
+		"sad" : "images/mouth/sad.png",
+		"surprised" : "images/mouth/surprised.png",
+		"disgusted": "images/mouth/disgusted.png",
+		"open" : "images/mouth/open.png"
+	},
+	"nose" :
+	{
+		"angry" : "images/nose/angry.png",
+		"fearful" : "images/nose/fearful.png",
+		"happy" : "images/nose/happy.png",
+		"neutral" : "images/nose/neutral.png",
+		"sad" : "images/nose/sad.png",
+		"surprised" : "images/nose/surprised.png",
+		"disgusted": "images/nose/disgusted.png"
+	},
+	"right_ear" : "images/right_ear.png",
+	"right_eye" :
+	{
+		"angry" : "images/right_eye/angry.png",
+		"fearful" : "images/right_eye/fearful.png",
+		"happy" : "images/right_eye/happy.png",
+		"neutral" : "images/right_eye/neutral.png",
+		"sad" : "images/right_eye/sad.png",
+		"surprised" : "images/right_eye/surprised.png",
+		"disgusted": "images/right_eye/disgusted.png",
+		"close": "images/right_eye/close.png"
+	},
+	"right_eyebrow" :
+	{
+		"angry" : "images/right_eyebrow/angry.png",
+		"fearful" : "images/right_eyebrow/fearful.png",
+		"happy" : "images/right_eyebrow/happy.png",
+		"neutral" : "images/right_eyebrow/neutral.png",
+		"sad" : "images/right_eyebrow/sad.png",
+		"surprised" : "images/right_eyebrow/surprised.png",
+		"disgusted": "images/right_eyebrow/disgusted.png"
+	},
+	"right_red" : "images/right_red.png"
+};
